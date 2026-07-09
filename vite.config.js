3b25adc0e678139f6f397df175e2628c2c42c4a5
HS001YT <singlaharshit480@gmail.com>
2026-07-09 11:06:06 +0530

    Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Online-Crime-Reporting-System/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
